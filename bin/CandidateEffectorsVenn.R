2b906e67496c83e1515162c50ace777af0290db4
Jamie Pike <62067579+JamiePike@users.noreply.github.com>
2023-12-30 17:27:01 +0000

    updates to candidate effector venn scriot

--- bin/CandidateEffectorsVenn.R
+++ bin/CandidateEffectorsVenn.R
@@ -5,7 +5,9 @@
 # install.packages("venn")
 
 # Load the venn library
-library(venn)
+library(ggplot2)
+library(ggvenn)
+
 
 # Read the TSV file
 data <- read.table("/Users/u1983390/Projects/NewToolsProject/exp/Effectors/cdhit/AllCandidateEffectorSets.clstr_heatmapdata.tsv", header = TRUE, sep = "\t", row.names = 1)
@@ -13,5 +15,17 @@ data <- read.table("/Users/u1983390/Projects/NewToolsProject/exp/Effectors/cdhit
 # Create sets for the Venn diagram
 sets <- lapply(data, function(col) rownames(data)[col == 1])
 
-# Display the Venn diagram
-venn(sets, category.names = c("SY-2", "S16", "S32", "S6"))
+# Convert sets to a format suitable for ggvenn
+ggvenn_data <- list(
+  S16 = sets[[1]],
+  S32 = sets[[2]],
+  S6 = sets[[3]],
+  SY_2 = sets[[4]]
+)
+
+# Create the Venn diagram using ggplot2 and ggvenn
+ggvenn_plot <- ggvenn(ggvenn_data, fill_color = c("#0073C2FF", "#EFC000FF", "#868686FF", "#CD534CFF"),
+  stroke_size = 0.5, set_name_size = 4)
+
+# Save the Venn diagram
+ggsave("/Volumes/Jamie_EXT/Projects/NewToolsProject/exp/Effectors/cdhit/sharedCandEffsVenn.png", plot = ggvenn_plot, width = 8, height = 6, units = "in")
